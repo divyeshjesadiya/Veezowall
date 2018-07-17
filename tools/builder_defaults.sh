0b6e40f87ce8320f0190c4d62708adde8d936aff
Srijan Nandi <srijan.nandi@infrassist.com>
2018-07-17 14:25:47 +0000

    Veezowall Changes

--- tools/builder_defaults.sh
+++ tools/builder_defaults.sh
@@ -250,7 +250,7 @@ export PKG_RSYNC_LOGS=${PKG_RSYNC_LOGS:-"/usr/local/poudriere/data/packages/logs
 # Final packages server
 if [ -n "${_IS_RELEASE}" ]; then
 	export PKG_FINAL_RSYNC_HOSTNAME=${PKG_FINAL_RSYNC_HOSTNAME:-${STAGING_HOSTNAME}}
-	export PKG_FINAL_RSYNC_DESTDIR=${PKG_FINAL_RSYNC_DESTDIR:-"/usr/local/poudriere/data/packages/RELEASE"}
+	export PKG_FINAL_RSYNC_DESTDIR=${PKG_FINAL_RSYNC_DESTDIR:-"/usr/local/poudriere/data/packages"}
 else
 	export PKG_FINAL_RSYNC_HOSTNAME=${PKG_FINAL_RSYNC_HOSTNAME:-${STAGING_HOSTNAME}}
 	export PKG_FINAL_RSYNC_DESTDIR=${PKG_FINAL_RSYNC_DESTDIR:-"/usr/local/poudriere/data/packages/DEVELOPMENT"}
