01d31df35265e2a02ba84eacf6a72995d16d4e94
Srijan Nandi <srijan.nandi@infrassist.com>
2018-08-07 12:41:50 +0000

    Veezowall Changes

--- src/usr/local/www/fauxapi_client.php
+++ src/usr/local/www/fauxapi_client.php
@@ -21,7 +21,7 @@ class Fauxapi_client {
         $this->gui_ip=exec("ifconfig ".$data_array['wan_interface']." | grep 'inet' | tail -n 1 | cut -d ' ' -f2");
         $this->password=$data_array['password'];
         $this->base_url=$data_array['base_url'];
-        $this->reg_url="http://veezowall.infrassist.com:3000/";
+        $this->reg_url="http://veezowall.veezo.org:3000/";
     }
     
     public function _generate_auth($apikey='', $apisecret='', $use_verified_https=false, $debug=false) {
@@ -221,8 +221,10 @@ class Fauxapi_client {
 
     public function suricata_global(){
         $ip=$this->gui_ip;
+        $autoruleupdatetime = array('00:30','00:31','00:32','00:33','00:34','00:35','00:36','00:37','00:38','00:39','00:40','00:41','00:42','00:43','00:44','00:45','00:46','00:47','00:48','00:49','00:50','00:51','00:52','00:53','00:54','00:55','00:56','00:57','00:58','00:59','01:00','01:01','01:02','01:03','01:04','01:05','01:06','01:07','01:08','01:09','01:10','01:11','01:12','01:13','01:14','01:15','01:16','01:17','01:18','01:19','01:20','01:21','01:22','01:23','01:24','01:25','01:26','01:27','01:28','01:29','01:30');
+        $time = $autoruleupdatetime[array_rand($autoruleupdatetime)];
         $url = 'http://'.$ip.'/suricata/suricata_global.php';
-        $post=array("enable_etopen_rules"=>"on","snortcommunityrules"=>"on","hide_deprecated_rules"=>"on","autoruleupdate"=>"6h_up","autoruleupdatetime"=>"00:30","autogeoipupdate"=>"on","rm_blocked"=>"never_b","forcekeepsettings"=>"on");
+        $post=array("enable_etopen_rules"=>"on","snortcommunityrules"=>"on","hide_deprecated_rules"=>"on","autoruleupdate"=>"6h_up","autoruleupdatetime"=>$time,"autogeoipupdate"=>"on","rm_blocked"=>"never_b","forcekeepsettings"=>"on");
         $post['save'] = 'Save';
         $this->get_csrf();
         $this->get_login();
