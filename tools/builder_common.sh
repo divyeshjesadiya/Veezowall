80fb870d41d50c904a97da8432a7edea9484f9cb
Srijan Nandi <srijan.nandi@infrassist.com>
2018-09-17 11:17:52 +0000

    Veezowall Changes

--- tools/builder_common.sh
+++ tools/builder_common.sh
@@ -1029,8 +1029,8 @@ create_memstick_image() {
 	echo ">>> Creating memstick to ${_image_path}." 2>&1 | tee -a ${LOGFILE}
 	echo "/dev/ufs/${PRODUCT_NAME} / ufs ro 0 0" > ${FINAL_CHROOT_DIR}/etc/fstab
 	echo "kern.cam.boot_delay=10000" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
-	echo "hw.igb.rxd=2048" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
-	echo "hw.igb.txd=2048" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
+	echo "hw.igb.rxd=4096" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
+	echo "hw.igb.txd=4096" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
 	
 	create_distribution_tarball
 
@@ -1070,8 +1070,8 @@ create_memstick_serial_image() {
 
 	echo "/dev/ufs/${PRODUCT_NAME} / ufs ro 0 0" > ${FINAL_CHROOT_DIR}/etc/fstab
 	echo "kern.cam.boot_delay=10000" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
-	echo "hw.igb.rxd=2048" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
-	echo "hw.igb.txd=2048" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
+	echo "hw.igb.rxd=4096" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
+	echo "hw.igb.txd=4096" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
 
 	echo ">>> Creating serial memstick to ${MEMSTICKSERIALPATH}." 2>&1 | tee -a ${LOGFILE}
 
@@ -1132,8 +1132,8 @@ create_memstick_adi_image() {
 
 	echo "/dev/ufs/${PRODUCT_NAME} / ufs ro 0 0" > ${FINAL_CHROOT_DIR}/etc/fstab
 	echo "kern.cam.boot_delay=10000" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
-	echo "hw.igb.rxd=2048" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
-	echo "hw.igb.txd=2048" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
+	echo "hw.igb.rxd=4096" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
+	echo "hw.igb.txd=4096" >> ${FINAL_CHROOT_DIR}/boot/loader.conf.local
 
 	echo ">>> Creating serial memstick to ${MEMSTICKADIPATH}." 2>&1 | tee -a ${LOGFILE}
 
